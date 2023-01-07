05bb624242cec28069801bd6d60ada3a7c361531
snickerbockers <snickerbockers@washemu.org>
2023-01-07 17:00:43 -0500

    save logs to a directory indicated by the logdir field in the config

--- gamma.pl
+++ gamma.pl
@@ -3,7 +3,7 @@
 ################################################################################
 #
 #
-#     Copyright (C) 2022 snickerbockers
+#     Copyright (C) 2022, 2023 snickerbockers
 #
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
@@ -25,16 +25,40 @@ use v5.20;
 use POSIX qw(strftime);
 use File::Path qw(make_path);
 use File::Copy;
+use Capture::Tiny 'tee_merged';
+
+my $logfile;
 
 sub on_branch_fail {
     my ($branch, $reason) = @_;
 
-    say STDERR "****************************************";
-    say STDERR "*****";
-    say STDERR "***** ERROR: FAILURE TO REBUILD $branch";
-    say STDERR "***** $reason";
-    say STDERR "*****";
-    say STDERR "****************************************";
+    say $logfile "****************************************";
+    say $logfile "*****";
+    say $logfile "***** ERROR: FAILURE TO REBUILD $branch";
+    say $logfile "***** $reason";
+    say $logfile "*****";
+    say $logfile "****************************************";
+}
+
+sub log_cmd {
+    my ($cmd, $logfiles) = @_;
+    my ($txt, $exit) = tee_merged {system("$cmd");};
+    for my $file (@{$logfiles}) {
+        say $file "****************************************";
+        say $file "*****";
+        say $file "***** EXECUTE COMMAND \"$cmd\"";
+        say $file "*****";
+        say $file "****************************************\n";
+
+        print $file $txt;
+
+        say $file "****************************************";
+        say $file "*****";
+        say $file "***** COMMAND EXIT $exit";
+        say $file "*****";
+        say $file "****************************************\n";
+    }
+    return $exit;
 }
 
 my $starttime = time();
@@ -43,7 +67,6 @@ my $cfg_path = $ARGV[0];
 
 my $datestring = strftime "%m_%d_%y", localtime;
 
-say "datestring is $datestring";
 say "configure file will be read from $cfg_path";
 
 open my $cfgfile, '<', $cfg_path or die "unable to open \"$cfg_path\"";
@@ -67,8 +90,17 @@ for (<$cfgfile>) {
     $cfg{$key} = $val;
 }
 
-say "build directory is $cfg{builddir}";
-say "publish directory is $cfg{pubdir}";
+my $logdir = $cfg{'logdir'};
+make_path $logdir;
+my $logfile_name = strftime "%m_%d_%y_%H-%M-%S.txt", localtime;
+my $logfile_path = "$logdir/$logfile_name";
+say "the logfile will be \"$logfile_path\"!";
+open $logfile, '>', $logfile_path or die "unable to open $logfile_path";
+
+say $logfile "datestring is $datestring";
+
+say $logfile "build directory is $cfg{builddir}";
+say $logfile "publish directory is $cfg{pubdir}";
 
 make_path $cfg{'builddir'};
 chdir $cfg{'builddir'};
@@ -76,9 +108,9 @@ chdir $cfg{'builddir'};
 exists $cfg{'branches'} or die 'no branches to build';
 exists $cfg{'repo'} or die 'no git repository supplied';
 
-say "the following branches will be built:";
+say $logfile "the following branches will be built:";
 for (@{$cfg{'branches'}}) {
-    say "\t$_";
+    say $logfile "\t$_";
 }
 
  build_branch:
@@ -87,16 +119,16 @@ for my $branch (@{$cfg{'branches'}}) {
     make_path $branchdir;
 
     chdir $cfg{'builddir'};
-    say "checking out $branch";
+    say $logfile "checking out $branch";
     my $dir = "gamma_$branch";
     `rm -rf $dir`;
-    if (system('git', 'clone', '--recursive', "$cfg{repo}", $dir) != 0) {
+    if (log_cmd("git clone --recursive $cfg{repo} $dir", [$logfile]) != 0) {
         on_branch_fail($branch, "unable to clone");
         next;
     }
 
     chdir $dir;
-    if (system('git', 'checkout', "$branch") != 0) {
+    if (log_cmd("git checkout $branch", [$logfile]) != 0) {
         on_branch_fail($branch, "failure to checkout branch");
         next;
     }
@@ -113,21 +145,21 @@ for my $branch (@{$cfg{'branches'}}) {
     if (opendir(my $branchdir_handle, $branchdir)) {
         while (my $buildstamp = readdir($branchdir_handle)) {
             next if ($buildstamp eq '.' || $buildstamp eq '..');
-            say "checking against build $buildstamp";
+            say $logfile "checking against build $buildstamp";
             my $hashfile = "$branchdir/$buildstamp/BUILD_HASH";
             if (-e $hashfile) {
                 open my $hashfile_handle, '<', $hashfile;
                 my $oldhash = <$hashfile_handle>;
                 close $hashfile_handle;
                 chomp $oldhash;
-                say "comparing \"$hash\" to \"$oldhash\"";
+                say $logfile "comparing \"$hash\" to \"$oldhash\"";
                 if ($oldhash eq $hash) {
-                    say "Branch $branch build skipped because there is " .
+                    say $logfile "Branch $branch build skipped because there is " .
                         "already a build of hash $hash";
                     next build_branch;
                 }
             } else {
-                say "$buildstamp did not have a BUILD_HASH file";
+                say $logfile "$buildstamp did not have a BUILD_HASH file";
             }
         }
     }
@@ -139,22 +171,22 @@ for my $branch (@{$cfg{'branches'}}) {
         next;
     }
 
-    if (system('cmake', '..') != 0) {
+    if (log_cmd('cmake ..', [$logfile]) != 0) {
         on_branch_fail($branch, "failure to configure cmake");
         next;
     }
 
-    if (system('cmake', '--build', '.', '--target', 'package_source') != 0) {
+    if (log_cmd('cmake --build . --target package_source', [$logfile]) != 0) {
         on_branch_fail($branch, "failure to create source tarball");
         next;
     }
 
-    if (system('cmake', '--build', '.') != 0) {
+    if (log_cmd('cmake --build .', [$logfile]) != 0) {
         on_branch_fail($branch, "failure to build program");
         next;
     }
 
-    if (system('cpack', '.') != 0) {
+    if (log_cmd('cpack .', [$logfile]) != 0) {
         on_branch_fail($branch, "failure to package build");
         next;
     }
@@ -195,49 +227,48 @@ for my $branch (@{$cfg{'branches'}}) {
 
     # now upload to coverity (if enabled)
     if (grep(/$branch/, @{$cfg{'coverity_branches'}})) {
-        say "*******************************************";
-        say "********* ITS COVERITY TIME $branch *********";
-        say "*******************************************";
+        say $logfile "*******************************************";
+        say $logfile "********* ITS COVERITY TIME $branch *********";
+        say $logfile "*******************************************";
 
         my $checkoutdir = "$cfg{builddir}/gamma_$branch";
-        say "about to chdir to \"$checkoutdir\"";
+        say $logfile "about to chdir to \"$checkoutdir\"";
         chdir $checkoutdir;
         make_path 'coverity_build';
         chdir 'coverity_build';
 
-        if (system('cmake', '..') != 0) {
+        `cmake ..`;
+        if (log_cmd('cmake ..', [$logfile]) != 0) {
             on_branch_fail($branch, 'failure to configure cmake (coverity)');
             next;
         }
 
-        if (system($cfg{'coverity_cmd'}, '--dir', 'cov-int', 'cmake', '--build', '.') != 0) {
+        if (log_cmd("$cfg{'coverity_cmd'} --dir cov-int cmake --build .", [$logfile]) != 0) {
             on_branch_fail($branch, 'failure to build program with cov-build');
             next;
         }
 
         my $covfile = 'washingtondc-coverity-data.tgz';
-        if (system('tar', '-czf', $covfile, 'cov-int') != 0) {
+        if (log_cmd("tar -czf $covfile cov-int", [$logfile]) != 0) {
             on_branch_fail($branch, 'failure to create coverity tarball');
             next;
         }
 
-        if (system('curl',
-                   '--form', "token=$cfg{coverity_token}",
-                   '--form', "email=$cfg{coverity_email}",
-                   '--form', "file=\@$covfile",
-                   '--form', "version=\"$cfg{coverity_version}\"",
-                   '--form', "description=\"$cfg{coverity_description}\"",
-                   'https://scan.coverity.com/builds?project=WashingtonDC') != 0) {
+        if (log_cmd("curl --form token=$cfg{coverity_token} --form email=$cfg{coverity_email} --form file=\@$covfile --form version=\"$cfg{coverity_version}\" --form description=\"$cfg{coverity_description}\" https://scan.coverity.com/builds?project=WashingtonDC", [$logfile]) != 0) {
             on_branch_fail($branch, 'failure to upload tarball to coverity');
             next;
         }
 
-        say "************************************************";
-        say "********* COVERITY SUBMISSION COMPLETE *********";
-        say "************************************************";
+        say $logfile "************************************************";
+        say $logfile "********* COVERITY SUBMISSION COMPLETE *********";
+        say $logfile "************************************************";
     }
 }
 
 my $deltatime = time() - $starttime;
 
 say "All operations completed in $deltatime seconds";
+say $logfile "All operations completed in $deltatime seconds";
+
+close $cfgfile;
+close $logfile;
